813d7e354e7f50e14a02d2ab6dca9859671da61a
RubnMrtin <rubnmrtin95@gmail.com>
2016-06-20 19:12:39 +0200

    Actualizo mi programa_inicial.py tras es descanso

---
+++ programa_inicial.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#
+# 20 junio 2016
+# Primera clase del curso: "Programando en Python: curso completo"
+# del 20 al 24 de junio de 2016 en la Facultad de Ciencias - UGR
+# http://www.darwineventur.com/2016/05/programando-en-python-curso-completo.html
+#
+
+#
+# Rubén Martín Moreno
+#
+
+
+var1 = 14.0
+var2 = 8.0
+
+print var1 + var2
+print var1 - var2
+print var1 * var2
+print var1 / var2
+print var1 // var2
+print var1 % var2
+print 10.0 ** 2.0 + 2.0
+print 100.0 ** 12.0
+
+raw_input()
