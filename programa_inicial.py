72827f273bfb5a8fc898381bfdb7b6b1ef0cc73a
RubnMrtin <rubnmrtin95@gmail.com>
2016-06-20 20:46:48 +0200

    Actualizo al final del 1er día

--- programa_inicial.py
+++ programa_inicial.py
@@ -13,16 +13,49 @@
 #
 
 
-var1 = 14.0
-var2 = 8.0
+mi_tupla = ('cadena de texto', 15, 2.8, 'otro dato', 25)
 
-print var1 + var2
-print var1 - var2
-print var1 * var2
-print var1 / var2
-print var1 // var2
-print var1 % var2
-print 10.0 ** 2.0 + 2.0
-print 100.0 ** 12.0
+print mi_tupla
 
-raw_input()
+print mi_tupla[1]
+
+print mi_tupla[-1]
+print mi_tupla[2:4]
+
+print '\n'
+
+mi_tupla = 7.0,7,7L
+print mi_tupla.count(int(7))
+
+milista = [1, 2, 3, 'cuatro', '5']
+print milista
+
+milista.append(6)
+print milista
+
+milista.insert(0,0)
+print milista
+
+milista.remove(2)
+print milista
+
+print 2 in milista
+print 'cuatro' in milista
+
+print milista
+del milista[3:5] 
+print milista
+
+tupla = (1, 2)
+lista = [3, 4]
+suma = list(tupla) + lista
+print suma
+
+#DICCIONARIO
+diccionario = {'dia' : 20, 'mes' : 'junio', 'year' : 2016}
+print diccionario['dia']
+print diccionario
+
+#ITERADOS
+for clave, valor in diccionario.iteritems():
+	print 'El valor de la clave %s es %s' % (clave, valor)
